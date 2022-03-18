5deba7cd156741462393beadebb7e103abe6b78a
Joe Near <jnear@uvm.edu>
2022-03-18 15:10:55 -0400

    Clean up MNIST example

---
+++ mini_wizpl_torch.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from mini_wizpl import SecretTensor, Prim, print_emp
+
+old_relu = F.relu
+def my_relu(x):
+    #print('ReLU Override successful')
+    return Prim('relu', [x])
+
+F.relu = my_relu
+
+old_linear = F.linear
+def my_linear(inp, weight, bias):
+    #print('Linear Override successful')
+    return Prim('matplus', [Prim('matmul', [inp, SecretTensor(weight.T)]),
+                            SecretTensor(bias)])
+#return old_addmm(inp, mat1, mat2)
+F.linear = my_linear
+
+old_softmax = F.log_softmax
+def my_softmax(x, dim=1):
+    #print('Softmax Override successful')
+    return Prim('softmax', [x, dim])
+#return old_addmm(inp, mat1, mat2)
+F.log_softmax = my_softmax
+
+
--- mnist_wizpl.py
+++ mnist_wizpl.py
@@ -7,8 +7,7 @@ from torch.optim.lr_scheduler import StepLR
 
 from mini_wizpl import SecretTensor, Prim, print_emp
 from mnist_model import Net
-
-
+import mini_wizpl_torch
 
 model = Net()
 model.load_state_dict(torch.load('mnist.pt'))
@@ -17,37 +16,11 @@ model.eval()
 print('model architecture:')
 print(model)
 
+# Run the model on a secret input
 print('output on a test input:')
 test_input = SecretTensor(torch.randn(1, 784))
-#test_input = torch.randn(1, 784)
-#print(model(test_input))
-
-old_relu = F.relu
-def my_relu(x):
-    print('ReLU Override successful')
-    return Prim('relu', [x])
-
-F.relu = my_relu
-
-old_linear = F.linear
-def my_linear(inp, weight, bias):
-    print('Linear Override successful')
-    return Prim('matplus', [Prim('matmul', [inp, SecretTensor(weight.T)]),
-                            SecretTensor(bias)])
-#return old_addmm(inp, mat1, mat2)
-F.linear = my_linear
-
-old_softmax = F.log_softmax
-def my_softmax(x, dim=1):
-    print('Softmax Override successful')
-    return Prim('softmax', [x, dim])
-#return old_addmm(inp, mat1, mat2)
-F.log_softmax = my_softmax
-
-
-
-print('output after override:')
-
 output = model(test_input)
 print(output)
+
+# Compile the output to EMP
 print_emp(output, 'miniwizpl_test.cpp')
