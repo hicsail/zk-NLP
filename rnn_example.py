b855379b65941f90a94efe734a96d462bd8db0c8
willyang <willyang@stealthsoftwareinc.com>
2022-04-15 13:43:49 -0700

    replace rnn_example.py with rnn_example2.py and delete rnn_example2.py

--- rnn_example.py
+++ rnn_example.py
@@ -1,203 +1,240 @@
-import torch
-from torch import nn
+# Taken from: https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
+# The tutorial involves training and running an example RNN. This is an official PyTorch example.
 
-import numpy as np
+from __future__ import unicode_literals, print_function, division
+from io import open
+import glob
+import os
+import torch
+
+def findFiles(path): return glob.glob(path)
+
+print(findFiles('data/names/*.txt'))
+
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+print(unicodeToAscii('Ślusàrski'))
+
+# Build the category_lines dictionary, a list of names per language
+category_lines = {}
+all_categories = []
+
+# Read a file and split into lines
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+n_categories = len(all_categories)
+print(category_lines['Italian'][:5])
+
+
+
+print(letterToTensor('J'))
+
+print(lineToTensor('Jones').size())
+
+import torch.nn as nn
+import torch.nn.functional as F
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        # self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        # output = self.softmax(output)
+        output = F.log_softmax(output, dim=1)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
+
+input = letterToTensor('A')
+hidden = torch.zeros(1, n_hidden)
+print(input)
+output, next_hidden = rnn(input, hidden)
+
+input = lineToTensor('Albert')
+hidden = torch.zeros(1, n_hidden)
+
+output, next_hidden = rnn(input[0], hidden)
+print(output)
+
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+print(categoryFromOutput(output))
+
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+
+criterion = nn.NLLLoss()
+
+learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+
+    rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item()
+
+import time
+import math
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+# Just return an output given a line
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output, hidden
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output, hidden = evaluate(lineToTensor(input_line))
+        print(output)
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+rnn.eval()
+predict('Yang')
+# predict('Jackson')
+# predict('Satoshi')
 
 from mini_wizpl import SecretTensor, Prim, print_emp
 
-def one_hot_encode(sequence, dict_size, seq_len, batch_size):
-    # Creating a multi-dimensional array of zeros with the desired output shape
-    features = np.zeros((batch_size, seq_len, dict_size), dtype=np.float32)
-    
-    # Replacing the 0 at the relevant character index with a 1 to represent that character
-    for i in range(batch_size):
-        for u in range(seq_len):
-            features[i, u, sequence[i][u]] = 1
-    return features
+# Initialize model
+print("model architecture: ")
+print(rnn)
 
-def predict(model, character):
-    # One-hot encoding our input to fit into the model
-    character = np.array([[char2int[c] for c in character]])
-    character = one_hot_encode(character, dict_size, character.shape[1], 1)
-    character = torch.from_numpy(character)
-    character = character.to(device)
-    
-    out, hidden = model(character)
+# Initialize secret input
+import mini_wizpl_torch
+input_str = "Yang"
+secret_hidden = SecretTensor(rnn.initHidden())
+for c in input_str:
+    # Turn each character into a one-hot encoded matrix
+    # and turn each matrix into a SecretTensor.
+    secret_input = SecretTensor(lineToTensor(c)[0])
+    out, secret_hidden = rnn(secret_input, secret_hidden)
 
-    prob = nn.functional.softmax(out[-1], dim=0).data
-    # print(prob)
-    # Taking the class with the highest probability score from the output
-    char_ind = torch.max(prob, dim=0)[1].item()
+print("output on a test input:")
+print(out)
 
-    return int2char[char_ind], hidden
-
-def sample(model, out_len, start='hey'):
-    model.eval() # eval mode
-    start = start.lower()
-    # First off, run through the starting characters
-    chars = [ch for ch in start]
-    size = out_len - len(chars)
-    # Now pass in the previous characters and get a new one
-    for ii in range(size):
-        char, h = predict(model, chars)
-        chars.append(char)
-
-    return ''.join(chars)
-
-def init_model_for_emp(model, input_str):
-    model.eval() # eval mode
-    input_str = input_str.lower()
-    characters = [_ for _ in input_str]
-    for i, character in enumerate(characters):
-        # One-hot encoding our input to fit into the model
-        character = np.array([[char2int[c] for c in character]])
-        character = one_hot_encode(character, dict_size, character.shape[1], 1)
-        character = torch.from_numpy(character)
-        character = character.to(device)
-        out, hidden = model(character) 
-        if i == (len(characters) - 1):
-            return out
-
-def init_input_str_for_emp(input_str):
-    chars = [c for c in input_str]
-    print(chars)
-    # One-hot encoding our input to fit into the model
-    chars = np.array([[char2int[c] for c in chars]])
-    chars = one_hot_encode(chars, dict_size, chars.shape[1], 1)
-    chars = torch.from_numpy(chars)
-    chars = chars.to(device)
-    return chars
-
-
-class Model(nn.Module):
-    def __init__(self, input_size, output_size, hidden_dim, n_layers):
-        super(Model, self).__init__()
-
-        # Defining some parameters
-        self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-
-        #Defining the layers
-        # RNN Layer
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
-        # Fully connected layer
-        self.fc = nn.Linear(hidden_dim, output_size)
-    
-    def forward(self, x):
-        
-        batch_size = x.size(0)
-
-        #Initializing hidden state for first input using method defined below
-        hidden = self.init_hidden(batch_size)
-
-        # Passing in the input and hidden state into the model and obtaining outputs
-        out, hidden = self.rnn(x, hidden)
-        
-        # Reshaping the outputs such that it can be fit into the fully connected layer
-        out = out.contiguous().view(-1, self.hidden_dim)
-        out = self.fc(out)
-        
-        return out, hidden
-    
-    def init_hidden(self, batch_size):
-        # This method generates the first hidden state of zeros which we'll use in the forward pass
-        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(device)
-         # We'll send the tensor holding the hidden state to the device we specified earlier as well
-        return hidden
-
-
-text = ['hey how are you','good i am fine','have a nice day', 'thanks you too', 'catch you later', 'see ya']
-
-# Join all the sentences together and extract the unique characters from the combined sentences
-chars = set(''.join(text))
-
-# Creating a dictionary that maps integers to the characters
-int2char = dict(enumerate(chars))
-
-# Creating another dictionary that maps characters to integers
-char2int = {char: ind for ind, char in int2char.items()}
-
-maxlen = len(max(text, key=len))
-print("The longest string has {} characters".format(maxlen))
-
-# Padding
-
-# A simple loop that loops through the list of sentences and adds a ' ' whitespace until the length of the sentence matches
-# the length of the longest sentence
-for i in range(len(text)):
-    while len(text[i])<maxlen:
-        text[i] += ' '
-
-
-# Creating lists that will hold our input and target sequences
-input_seq = []
-target_seq = []
-
-for i in range(len(text)):
-    # Remove last character for input sequence
-    input_seq.append(text[i][:-1])
-    
-    # Remove firsts character for target sequence
-    target_seq.append(text[i][1:])
-    print("Input Sequence: {}\nTarget Sequence: {}".format(input_seq[i], target_seq[i]))
-
-for i in range(len(text)):
-    input_seq[i] = [char2int[character] for character in input_seq[i]]
-    target_seq[i] = [char2int[character] for character in target_seq[i]]
-
-dict_size = len(char2int)
-seq_len = maxlen - 1
-batch_size = len(text)
-
-input_seq = one_hot_encode(input_seq, dict_size, seq_len, batch_size)
-print("Input shape: {} --> (Batch Size, Sequence Length, One-Hot Encoding Size)".format(input_seq.shape))
-
-input_seq = torch.from_numpy(input_seq)
-target_seq = torch.Tensor(target_seq)
-
-# torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
-is_cuda = torch.cuda.is_available()
-
-# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
-if is_cuda:
-    device = torch.device("cuda")
-    print("GPU is available")
-else:
-    device = torch.device("cpu")
-    print("GPU not available, CPU used")
-
-# Instantiate the model with hyperparameters
-model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=12, n_layers=1)
-# We'll also set the model to the device that we defined earlier (default is CPU)
-model = model.to(device)
-
-# Define hyperparameters
-n_epochs = 100
-lr=0.01
-
-# Define Loss, Optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-# Training Run
-input_seq = input_seq.to(device)
-for epoch in range(1, n_epochs + 1):
-    optimizer.zero_grad() # Clears existing gradients from previous epoch
-    #input_seq = input_seq.to(device)
-    output, hidden = model(input_seq)
-    output = output.to(device)
-    target_seq = target_seq.to(device)
-    loss = criterion(output, target_seq.view(-1).long())
-    loss.backward() # Does backpropagation and calculates gradients
-    optimizer.step() # Updates the weights accordingly
-    
-    if epoch%10 == 0:
-        print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
-        print("Loss: {:.4f}".format(loss.item()))
-
-print(sample(model, 15, 'hey'))
-out = init_model_for_emp(model, "hey")
-input_tensor = init_input_str_for_emp("hey")
-print(input_tensor)
-SecretTensor(input_tensor)
+# Print EMP
 print_emp(out, 'rnn_test.cpp')
