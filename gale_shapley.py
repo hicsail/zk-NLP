687a4ed1e0a3360df5d6e7bfa1052c687fac119b
Joe Near <jnear@uvm.edu>
2022-05-27 16:59:49 -0400

    python gale-shapley

---
+++ gale_shapley.py
@@ -0,0 +1,44 @@
+
+men = [0, 1, 2]
+women = [3, 4, 5]
+
+preference_lists = {
+    0: [3,4,5],
+    1: [3,4,5],
+    2: [3,4,5],
+    3: [2,1,0],
+    4: [2,1,0],
+    5: [0,1,2]
+}
+
+def prefers(w, m, m_p):
+    pl = preference_lists[w]
+    p = pl.index(m) < pl.index(m_p)
+    print(f'does {w} prefer {m} to {m_p}? {p}')
+    return p
+
+def gale_shapley():
+    unmarried_men = set(men)
+    marriages = {}
+    next_proposal = [0 for _ in men]
+
+    while unmarried_men:
+        m = unmarried_men.pop()
+        w = preference_lists[m][next_proposal[m]]
+        next_proposal[m] += 1
+        print(marriages)
+        print(f'{m} proposes to {w}')
+
+        if w not in marriages:
+            marriages[w] = m
+        elif prefers(w, m, marriages[w]):
+            unmarried_men.add(marriages[w])
+            marriages[w] = m
+        else:
+            unmarried_men.add(m)
+    return marriages
+
+print(gale_shapley())
+            
+
+    
