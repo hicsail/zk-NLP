07dae9d1439797cde07f0151c3f0f902b94594d4
Joe Near <jnear@uvm.edu>
2022-06-06 16:10:03 -0400

    add a note and counterexample to current compiler approach

--- stack_example.py
+++ stack_example.py
@@ -1,7 +1,13 @@
 from miniwizpl import SecretInt, SecretStack, print_emp
 
+# NOTE: we currently don't handle ordering correctly here, due to the way
+# we capture side effects and how they can interleave with assignments
+
 xs = SecretStack([1,2,3])
+
 xs.push(5)
 y = xs.pop()
+xs.push(6)
+z = xs.pop()
 
 print_emp(y, 'miniwizpl_test.cpp')
