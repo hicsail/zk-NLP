2859dc4e3d94fbbc18262167cbdc939b5b5e1ed2
Joe Near <jnear@uvm.edu>
2022-09-15 12:56:09 -0400

    fix mux example

--- examples/simple_demos/mux_ir1.py
+++ examples/simple_demos/mux_ir1.py
@@ -1,4 +1,4 @@
-from miniwizpl import SecretInt, Prim, print_ir1, assert0, set_field
+from miniwizpl import SecretInt, Prim, print_ir1, assert0, set_field, mux
 set_field(2**61 - 1)
 
 x = SecretInt(3)
