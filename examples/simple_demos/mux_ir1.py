f63ec5273b27dc90f97cc5b45473d327aea5ac14
Joe Near <jnear@uvm.edu>
2022-09-15 12:55:55 -0400

    add mux example

---
+++ examples/simple_demos/mux_ir1.py
@@ -0,0 +1,12 @@
+from miniwizpl import SecretInt, Prim, print_ir1, assert0, set_field
+set_field(2**61 - 1)
+
+x = SecretInt(3)
+y = SecretInt(2)
+a = SecretInt(20)
+b = SecretInt(30)
+z = mux(x == y, a, b+a)
+output = z
+assert0(output - 30)
+
+print_ir1("miniwizpl_test")
