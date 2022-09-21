427bce488bce4cd427d89232658ad887a18dab91
Snafkin547 <harunobuishii@gmail.com>
2022-09-21 18:30:15 -0400

    test file added

---
+++ miniwizpl_test.cpp
@@ -0,0 +1,112 @@
+#include "emp-zk/emp-zk.h"
+#include <iostream>
+#include "emp-tool/emp-tool.h"
+#include "emp-zk/emp-zk/extensions/ram-zk/zk-mem.h"
+#include "matrix.h"
+using namespace emp;
+using namespace std;
+
+int port, party;
+const int threads = 1;
+const int index_sz = 20, step_sz = 25, val_sz = 32;
+
+QSMatrix<Float> relu(const QSMatrix<Float>& mat) {
+  unsigned rows = mat.get_rows();
+  unsigned cols = mat.get_cols();
+
+  QSMatrix<Float> result(rows, cols, 0.0);
+  Float zero = Float(0.0, PUBLIC);
+
+  for (unsigned i=0; i<rows; i++) {
+    for (unsigned j=0; j<cols; j++) {
+      Float val = mat(i, j);
+      Bit t = val.less_equal(zero);
+      result(i,j) = val.If(t, zero);
+    }
+  }
+
+  return result;
+}
+
+QSMatrix<Float> log_softmax(const QSMatrix<Float>& mat) {
+  unsigned rows = mat.get_rows();
+  unsigned cols = mat.get_cols();
+
+  QSMatrix<Float> result(rows, cols, 0.0);
+
+  Float sum = Float(0.0, PUBLIC);
+  for (unsigned i=0; i<rows; i++) {
+    for (unsigned j=0; j<cols; j++) {
+      sum = sum + mat(i, j).exp();
+    }
+  }
+
+  for (unsigned i=0; i<rows; i++) {
+    for (unsigned j=0; j<cols; j++) {
+      result(i, j) = (mat(i, j).exp() / sum).ln();
+    }
+  }
+
+  return result;
+}
+
+bool compare_qs_matrices(const QSMatrix<Float>& a, const QSMatrix<Float>& b) {
+  if (a.get_rows() != b.get_rows()) return false;
+  if (a.get_cols() != b.get_cols()) return false;
+
+  for (unsigned i = 0; i < a.get_rows(); i++) {
+    for (unsigned j = 0; j < a.get_cols(); j++) {
+      if (a(i, j).reveal<double>(PUBLIC) - b(i, j).reveal<double>(PUBLIC) > .001) return false;
+    }
+  }
+  return true;
+}
+
+Integer mux(Bit s, Integer a, Integer b) {
+  return b.select(s, a);
+}
+
+// *************************************************************************
+
+void test(BoolIO<NetIO> *ios[threads], int party) {
+  setup_zk_bool<BoolIO<NetIO>>(ios, threads, party);
+
+  cout << "!!\n";
+
+  Float pub_zero = Float(0.0, PUBLIC);
+
+  cout << "starting defs\n";
+  Integer intval_1 = Integer(32, 3, ALICE);
+
+  Integer intval_2 = Integer(32, 2, ALICE);
+
+
+  cout << "defs complete\n";
+
+  Integer result_3 = intval_1 + intval_2;
+  int final_result = result_3.reveal<int>(PUBLIC);
+  cout << "final result:" << final_result << "\n";
+
+  int num_and_gates = CircuitExecution::circ_exec->num_and();
+  cout << "Number of and gates executed: " << num_and_gates << "\n";
+  finalize_zk_bool<BoolIO<NetIO>>();
+  cout <<"done\n";
+}
+
+
+
+int main(int argc, char** argv) {
+  parse_party_and_port(argv, &party, &port);
+  BoolIO<NetIO>* ios[threads];
+  for(int i = 0; i < threads; ++i)
+    ios[i] = new BoolIO<NetIO>(new NetIO(party == ALICE?nullptr:"127.0.0.1",port), party==ALICE);
+
+  test(ios, party);
+
+  for(int i = 0; i < threads; ++i) {
+    delete ios[i]->io;
+    delete ios[i];
+  }
+  return 0;
+}
+
