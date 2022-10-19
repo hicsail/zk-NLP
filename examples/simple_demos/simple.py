ae95462514aef530f1f151d60e4241fff7bf1204
Mark Blunk <mark@stealthsoftwareinc.com>
2022-10-19 14:36:19 -0700

    changing output back to integer from boolean to use assert0EMP

--- examples/simple_demos/simple.py
+++ examples/simple_demos/simple.py
@@ -2,7 +2,7 @@ from miniwizpl import SecretInt, Prim, assert0EMP, print_emp
 
 x = SecretInt(3)
 y = SecretInt(2)
-output = x + y == 5
+output = x + y - 5
 
 assert0EMP(output)
 print_emp(output, "miniwizpl_test.cpp")
