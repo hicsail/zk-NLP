76d7de97d2773efd0a74ddb529e96d5a42805201
emma-shroyer <eshroyer@uvm.edu>
2022-11-03 11:13:54 -0400

    Work for the poseidon hash function

---
+++ examples/poseidon_hash/poseidon_hash.py
@@ -0,0 +1,13 @@
+from miniwizpl import *
+
+poseidon_simple, t = case_simple()
+
+input_vec = SecretList([x for x in range(0, t)])
+
+# [SecretInt(x) for x in range(0, t)]
+print("Input: ", input_vec)
+#
+poseidon_digest = poseidon_simple.run_hash(input_vec)
+assert0(len(input_vec.val) - 9)
+print(poseidon_digest)
+print_ir1("miniwizpl_test_ir1")
