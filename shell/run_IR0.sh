bb5c48184fcc278dde6a0de17efe71aac54cca12
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2024-02-23 15:36:22 -0500

    Update/shell (#40)
    * Shell script modified
    
    * Updated shell

--- shell/run_IR0.sh
+++ shell/run_IR0.sh
@@ -1,8 +1,12 @@
 # # #!/bin/sh
 
+# Initialize variables
+file=""
+operation=""
+size=""
+target=""
 
 # Taking tags from the command line
-
 while getopts f:c:o:s:t: flag
 do
     case "${flag}" in
@@ -13,102 +17,61 @@ do
     esac
 done
 
-
-
 # Checking if a file name is properly specified
-
 if [ -z "$file" ]
     then
         echo "Please specify an object code"
         exit 1
 fi
 
+# Setting default values for missing parameters.
+: ${target:="./irs"}
+: ${size:=0}
 
-
-# Setting missing parameters if any
-
-if [ -z "$target" ]
-    then
-        target="./tests"
-        echo "test directory is set to './tests' "
-fi
-
-if [ -z "$size" ]
-    then
-        size=0
-        echo "test size is set to 0 "
-fi
-
-
-
-# Copying the latest util.py into the container
-
-local="/code/substring_search/common/util.py"
-container="/usr/src/app/examples/substring_search/common/util.py"
-cp $local $container
-
-
-
-# Copying a designated statement file
+# Inform the user of defaults only when they are used.
+[ -z "$OPTARG" ] && echo "Default test directory set to './irs'"
+[ -z "$OPTARG" ] && echo "Default test size set to 0"
 
 dir="/usr/src/app/examples/substring_search/IR0/"
-orig="/code/substring_search/IR0/"
-cp $orig$file.py $dir$file.py
-
-
 prime=2305843009213693951
 prime_fam="p1"
-
 underscore="_"
-name=$target/$file$underscore$prime_fam$underscore$size
-rel=$name.rel
-wit=$name.type0.wit
-ins=$name.type0.ins
+name="${target}/${file}${underscore}${prime_fam}${underscore}${size}"
+rel="${name}.rel"
+wit="${name}.type0.wit"
+ins="${name}.type0.ins"
 
-[ -e $rel  ] && rm $rel
-[ -e $wit  ] && rm $wit
-[ -e $ins  ] && rm $ins
-
+# Removing existing files if they exist.
+rm -f "$rel" "$wit" "$ins"
 
 
-# Actual Execution
-
+# Execution starts.
 echo "Running $file .... $operation test size:$size";
 
-if [ "$operation" = "test" ]
+# Handling different operations.
+if [ "$operation" = "test" ]; then
+    echo "Running synthetic test case"
+else
+    echo "Running with your own text input in debug mode"
+fi
 
-    then 
+python3 "${dir}${file}.py" "$target" "$prime" "$prime_fam" "$size" "$operation";
 
-        echo "Running synthetic test case"
 
-        if python3 $dir$file.py $target $prime $prime_fam $size "test"
-            then
-                if wtk-firealarm $rel $wit $ins
-                    then
-                        echo "wtk-firealarm successfully completed"
-                    else
-                        echo "Error during wtk-firealarm"
-                fi
-            else
-                echo "Error in the python script - abort"
-        fi
+# firealarm format check
 
-    else
+# Check if wtk-firealarm is installed.
+if ! command -v wtk-firealarm >/dev/null 2>&1; then
+    echo "wtk-firealarm is not installed. Please follow the instructions below to install it:"
+    echo "  git clone git@github.mit.edu:sieve-all/wiztoolkit.git"
+    echo "  cd wiztoolkit && make && make install"
+    echo "  cp /usr/src/app/wiztoolkit/target/wtk-firealarm /usr/bin/wtk-firealarm"
+    echo "  cd .. #Go back to the current directory"
+    exit 1
+fi
 
-        echo "Running with your own text input in debug mode"
-        cp /code/dfa_test_input.txt /usr/src/app/examples/dfa_test_input.txt
-
-        if python3 $dir$file.py $target $prime $prime_fam $size "debug"
-            then
-                echo 'Check the output above'
-                if wtk-firealarm $rel $wit $ins
-                    then
-                        echo "wtk-firealarm successfully completed"
-                    else
-                        echo "Error during wtk-firealarm"
-                fi
-            else
-                echo "Error in the python script - abort"
-        fi
-
+if wtk-firealarm "$rel" "$wit" "$ins"; then
+    echo "wtk-firealarm successfully completed"
+else
+    echo "Error during wtk-firealarm"
 fi
